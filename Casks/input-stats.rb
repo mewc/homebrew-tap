85f650326636a4a2181c3947b41df9ffe68c1ecc
mewc <michael@drummerduck.com>
2026-09-16 21:23:17 +1000

    input-stats 0.3.8

--- Casks/input-stats.rb
+++ Casks/input-stats.rb
@@ -1,6 +1,6 @@
 cask "input-stats" do
-  version "0.3.7"
-  sha256 "b8be00efeb1166d88e464bece93acabbc390dc93b8b3a51579b9cd2f80fe0763"
+  version "0.3.8"
+  sha256 "aeaffbb9aed54bfebe861761b92c8c06762cbac26f292f0e7d9e425a65755cfc"
 
   url "https://github.com/mewc/input-stats/releases/download/v#{version}/InputStats.zip"
   name "Input Stats"
